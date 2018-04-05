a0934d119cb2ee5e542d491d14af46e9261cce71
Zana Flander <zana.flander@gmail.com>
2018-04-05 15:05:03 +0200

    debugged if sb clicks on deck it does not count as click or move

--- js/app.js
+++ js/app.js
@@ -58,51 +58,54 @@ let interval;
 
 // set up the event listener for a card
 deck.addEventListener("click", function(e) {
-    // start timer on first click
-    startTimer();
+    //works only if clicking on cards, but not on space between cards
+    if (e.target.tagName === "LI") {
+        // start timer on first click
+        startTimer();
 
-    // display the card's symbol
-	e.target.classList.add("open");
+        // display the card's symbol
+    	e.target.classList.add("open");
 
-    // add the card to a *list* of "open" cards
-    openCards.push(e.target);
-    let cardPick1 = openCards[0];
-    let cardPick2 = openCards[1];
+        // add the card to a *list* of "open" cards
+        openCards.push(e.target);
+        let cardPick1 = openCards[0];
+        let cardPick2 = openCards[1];
 
-    //  if the list already has another card, check to see if the two cards match
-    if (openCards.length === 2) {
-        // if the cards match, lock the cards in the open position
-        if (cardPick1.innerHTML === cardPick2.innerHTML) {
-            cardPick1.classList.add("match");
-            cardPick2.classList.add("match");
-            cardPick1.classList.remove("open");
-            cardPick2.classList.remove("open");
-            matchCards.push(cardPick1);
-            matchCards.push(cardPick2);
-            openCards = [];
-        } else {
-        // if the cards do not match, remove the cards from the list and hide the card's symbol
-            setTimeout( function() {
+        //  if the list already has another card, check to see if the two cards match
+        if (openCards.length === 2) {
+            // if the cards match, lock the cards in the open position
+            if (cardPick1.innerHTML === cardPick2.innerHTML) {
+                cardPick1.classList.add("match");
+                cardPick2.classList.add("match");
                 cardPick1.classList.remove("open");
                 cardPick2.classList.remove("open");
+                matchCards.push(cardPick1);
+                matchCards.push(cardPick2);
                 openCards = [];
-            }, 500);
+            } else {
+            // if the cards do not match, remove the cards from the list and hide the card's symbol
+                setTimeout( function() {
+                    cardPick1.classList.remove("open");
+                    cardPick2.classList.remove("open");
+                    openCards = [];
+                }, 500);
+            }
+            
+            // counting moves and display on screen
+            moves++;
+            document.getElementById("moves").innerHTML = moves;
         }
-        
-        // counting moves and display on screen
-        moves++;
-        document.getElementById("moves").innerHTML = moves;
-    }
 
-    countClicks++;
+        countClicks++;
 
-    // function star rating: removing stars
-    removeStar();
+        // function star rating: removing stars
+        removeStar();
 
-    // if all cards have matched, display a message with the final score and stop timer
-    if (matchCards.length === 16) {
-        //end of game: stop timer + show pop-up message
-        showModal();
+        // if all cards have matched, display a message with the final score and stop timer
+        if (matchCards.length === 16) {
+            //end of game: stop timer + show pop-up message
+            showModal();
+        }
     }
 });
 
